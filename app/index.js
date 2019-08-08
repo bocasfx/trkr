cd8018db20ed19ba4d6e2a5e643fcc2c9c129274
Rodolfo Palacios <rodolfo@gobridgit.com>
2019-08-08 16:54:50 -0400

    Linting.

--- app/index.js
+++ app/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello World</div>
-        )
-    }
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
