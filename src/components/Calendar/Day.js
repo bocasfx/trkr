e08c45b9c14d4b9e4b2ce850bfd92e39c0ff3c74
Rodolfo Palacios <rodolfo@gobridgit.com>
2019-08-26 17:16:53 -0400

    Added Day component.

---
+++ src/components/Calendar/Day.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Day.css';
+
+const Day = ({ dateNumber, achievement, large }) => {
+
+  const toggleAchievement = () => {
+    if (dateNumber) {
+      console.log('toggle');
+    }
+  };
+
+  if (!dateNumber) {
+    return <div className="day__blank" />;
+  }
+
+  let buttonClass = 'day__button';
+  buttonClass += achievement ? ' day__achievement' : '';
+  buttonClass += large ? ' day__large' : '';
+
+  return (
+    <button type="button" className={buttonClass} onClick={toggleAchievement}>{dateNumber}</button>
+  );
+};
+
+Day.defaultProps = {
+  achievement: null,
+  dateNumber: null,
+  large: false,
+};
+
+Day.propTypes = {
+  large: PropTypes.bool,
+  dateNumber: PropTypes.number,
+  achievement: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    month: PropTypes.number.isRequired,
+    day: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    __typename: PropTypes.string.isRequired,
+  }),
+};
+
+export default Day;
--- src/components/Calendar/Month.js
+++ src/components/Calendar/Month.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Month.css';
 import PropTypes from 'prop-types';
 import { findAchievementIndex } from '../../utils';
+import Day from './Day';
 
 const Month = (props) => {
   const {
@@ -15,24 +16,20 @@ const Month = (props) => {
     let dateNumber = 1;
     const daysInMonth = 32 - new Date(year, month, 32).getDate();
     const firstDay = new Date(year, month).getDay();
-    const weekContainerClass = viewFullYear ? 'month__week-container' : 'month__week-container-large';
 
     const rows = [];
     for (let rowIdx = 0; rowIdx < 5; rowIdx++) {
       const cells = [];
       for (let j = 0; j < 7; j++) {
         if ((rowIdx === 0 && j < firstDay) || dateNumber > daysInMonth) {
-          cells.push(<div key={j} />);
+          cells.push(<Day key={j} />);
         } else {
-          const achievementClass = findAchievementIndex(achievements, dateNumber) >= 0 
-            ? 'month__achievement'
-            : '';
-
-          cells.push(<div className={achievementClass} key={j}>{dateNumber}</div>);
+          const achievementIdx = findAchievementIndex(achievements, dateNumber);
+          cells.push(<Day key={j} large={!viewFullYear} dateNumber={dateNumber} achievement={achievements[achievementIdx]} />);
           dateNumber++;
         }
       }
-      rows.push(<div key={rowIdx} className={weekContainerClass}>{cells}</div>);
+      rows.push(<div key={rowIdx} className="month__week-container">{cells}</div>);
     }
     return rows;
   };
