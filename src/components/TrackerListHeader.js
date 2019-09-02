be8999929ddf8c5556e5c50ce11640fd25336d87
rodolfo <rodolfo@design2code.ca>
2019-09-02 14:34:13 -0400

    Tracker list header animations.

--- src/components/TrackerListHeader.js
+++ src/components/TrackerListHeader.js
@@ -4,12 +4,16 @@ import './TrackerListHeader.css';
 const TrackerListHeader = () => {
   const [open, setOpen] = useState(false);
 
-  const renderForm = () => (
-    <div className="tracker_list_header__form">
-      <input />
-      <button type="button" onClick={() => setOpen(false)}>Add</button>
-    </div>
-  );
+  const renderForm = () => {
+    let formClass = 'tracker_list_header__form';
+    formClass += open ? ' tracker_list_header__form-open' : ' tracker_list_header__form-close';
+    return (
+      <div className={formClass}>
+        <input />
+        <button type="button" onClick={() => setOpen(false)}>Add</button>
+      </div>
+    )
+  };
 
   let buttonClass = 'btn-round animated ';
   buttonClass += open ? 'closeButton' : 'plusButton';
@@ -25,7 +29,7 @@ const TrackerListHeader = () => {
           <button type="button" className="tracker_list_header__add-button" onClick={() => setOpen(!open)}>+</button>
         </div> */}
       </div>
-      { open && renderForm() }
+      { renderForm() }
     </>
   );
 };
