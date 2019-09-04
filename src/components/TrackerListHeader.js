fa445060e713f2530974195d36fb9c5fe0a3cf16
Rodolfo Palacios <rodolfo@gobridgit.com>
2019-09-04 17:43:06 -0400

    Add tracker.

--- src/components/TrackerListHeader.js
+++ src/components/TrackerListHeader.js
@@ -4,12 +4,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createTracker } from '../sagas/trackers';
 
 const TrackerListHeader = () => {
-  const [open, setOpen] = useState(false);
+  const [state, setState] = useState({ open: false, trackerName: '' });
+  const { open, trackerName } = state;
   const dispatch = useDispatch();
-  const { id } = useSelector(state => state.user);
+  const { id } = useSelector(mainState => mainState.user);
 
   const addTracker = () => {
-    dispatch(createTracker('hello', id));
+    if (trackerName.length) {
+      dispatch(createTracker(trackerName, id));
+    }
+  };
+
+  const onKeyUp = (event) => {
+    setState({ trackerName: event.target.value, open: true });
   };
 
   const renderForm = () => {
@@ -17,10 +24,12 @@ const TrackerListHeader = () => {
     formClass += open ? ' tracker_list_header__form-open' : ' tracker_list_header__form-close';
     return (
       <div className={formClass}>
-        <input />
-        <button type="button" onClick={addTracker}>Add</button>
+        <input onKeyUp={onKeyUp} />
+        <button type="button" onClick={addTracker}>
+          Add
+        </button>
       </div>
-    )
+    );
   };
 
   let buttonClass = 'btn-round animated ';
@@ -30,14 +39,17 @@ const TrackerListHeader = () => {
     <>
       <div className="tracker_list_header__container">
         <h1>Trackers</h1>
-        <div className={buttonClass} onClick={() => setOpen(!open)}>
+        <div
+          className={buttonClass}
+          onClick={() => setState({ open: !open })}
+          role="button"
+          onKeyPress={() => {}}
+          tabIndex={0}
+        >
           <span className="close" />
         </div>
-        {/* <div className={rotated}>
-          <button type="button" className="tracker_list_header__add-button" onClick={() => setOpen(!open)}>+</button>
-        </div> */}
       </div>
-      { renderForm() }
+      {renderForm()}
     </>
   );
 };
